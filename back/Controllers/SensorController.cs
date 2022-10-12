21a2888f089d68eb56e88c509f53d29ba814ba31
mc_lojek <mrcinekdbw@gmail.com>
2022-10-12 13:19:40 +0200

    backend initial project

---
+++ back/Controllers/SensorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dot.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SensorController: ControllerBase
+{
+    [HttpGet(Name= "GetSensorData")]
+    public String Get()
+    {
+        return "Hello world!";
+    }
+}
